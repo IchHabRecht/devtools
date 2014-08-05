e521fc294d040d45178c78d038e49c5dba0df988
Nicole Cordes <typo3@cordes.co>
2014-08-06 00:41:20 +0200

    [TASK] Add basis files

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "devtools".
+ *
+ * Auto generated 06-08-2014 00:40
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Developer tools',
+	'description' => 'Adds nice developer tools to your TYPO3 installation',
+	'category' => 'be',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Nicole Cordes',
+	'author_email' => 'typo3@cordes.co',
+	'author_company' => 'CPS-IT GmbH',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.7-0.0.0',
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:1:{s:12:"ext_icon.gif";s:4:"2712";}',
+	'suggests' => array(
+	),
+);
+
+?>
