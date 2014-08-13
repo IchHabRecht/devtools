b1fd7c9f5abe0b2c433cb0632eb0afd2c7a347a9
Nicole Cordes <typo3@cordes.co>
2014-08-13 20:25:08 +0200

    [BUGFIX] Add javascript action to dataTables draw listener

--- Resources/Public/Javascript/bindActions.js
+++ Resources/Public/Javascript/bindActions.js
@@ -1,25 +1,27 @@
 (function ($) {
 
-	$(document).ready(function() {
+	$(document).ready(function () {
 		bindActions();
+		$('#typo3-extension-list').on('draw.dt', bindActions);
 	});
 
 	function bindActions() {
-		$('.list-modified-files').each(function() {
+		$('.list-modified-files').not('.transformed').each(function () {
 			$(this).data('href', $(this).attr('href'));
 			$(this).attr('href', '#');
-			$(this).click(function() {
+			$(this).addClass('transformed');
+			$(this).click(function () {
 				$('.typo3-extension-manager').mask();
 				$.ajax({
 					url: $(this).data('href'),
-					success: function(data) {
+					success: function (data) {
 						$('.typo3-extension-manager').unmask();
 						TYPO3.Dialog.InformationDialog({
 							title: 'Test title',
 							msg: data.message
 						});
 					},
-					error: function() {
+					error: function () {
 						$('.typo3-extension-manager').unmask();
 					}
 				});
