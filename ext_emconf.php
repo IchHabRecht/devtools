b6cdb3ba950e0ee02caadb465b7cb6d4c5bfaad6
Nicole Cordes <typo3@cordes.co>
2014-08-14 22:36:57 +0200

    [RELEASE] Release of devtools 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,48 +3,39 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "devtools".
  *
- * Auto generated 06-08-2014 00:40
+ * Auto generated 14-08-2014 22:34
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Developer tools',
 	'description' => 'Adds nice developer tools to your TYPO3 installation',
 	'category' => 'be',
-	'shy' => 0,
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
-	'lockType' => '',
 	'author' => 'Nicole Cordes',
 	'author_email' => 'typo3@cordes.co',
 	'author_company' => 'CPS-IT GmbH',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
 	'version' => '0.1.0',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'php' => '5.3.7-0.0.0',
 			'typo3' => '6.2.0-6.2.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
-	'_md5_values_when_last_written' => 'a:1:{s:12:"ext_icon.gif";s:4:"2712";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"0083";s:12:"ext_icon.gif";s:4:"2712";s:17:"ext_localconf.php";s:4:"cbf8";s:50:"Classes/Controller/Slot/AbstractSlotController.php";s:4:"e355";s:83:"Classes/Controller/Slot/Extensionmanager/ProcessActions/ModifiedFilesController.php";s:4:"1a54";s:93:"Classes/Controller/Slot/Extensionmanager/ProcessActions/UpdateConfigurationFileController.php";s:4:"4eef";s:48:"Classes/Slot/Extensionmanager/ProcessActions.php";s:4:"457e";s:36:"Classes/Utility/ExtensionUtility.php";s:4:"ae5c";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"5363";s:26:"Documentation/Settings.yml";s:4:"03e2";s:46:"Documentation/Images/dialog_modified_files.jpg";s:4:"b064";s:40:"Documentation/Images/list_view_icons.jpg";s:4:"340f";s:36:"Documentation/Introduction/Index.rst";s:4:"69d5";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"c7c2";s:40:"Resources/Private/Language/locallang.xlf";s:4:"c492";s:42:"Resources/Public/Javascript/bindActions.js";s:4:"e9f3";}',
 );
 
-?>
