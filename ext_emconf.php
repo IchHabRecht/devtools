fecdf01a054f6db3adce7673165bd83ee8dfdb7f
Nicole Cordes <typo3@cordes.co>
2015-10-29 23:25:36 +0100

    [RELEASE] Release of devtools 0.2.0
    Change-Id: Id5876796dc39099d7f7791396ab3fcb7f6df5849

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,36 +3,38 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "devtools".
  *
- * Auto generated 12-11-2014 11:12
+ * Auto generated 29-10-2015 23:25
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-    'title' => 'Developer tools',
-    'description' => 'Adds nice developer tools to your TYPO3 installation',
-    'category' => 'be',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'author' => 'Nicole Cordes',
-    'author_email' => 'typo3@cordes.co',
-    'author_company' => 'CPS-IT GmbH',
-    'version' => '0.1.1',
-    'constraints' =>
-        array(
-            'depends' =>
-                array(
-                    'typo3' => '7.5.0-7.99.99',
-                ),
-            'conflicts' =>
-                array(),
-            'suggests' =>
-                array(),
-        ),
-    '_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"f68b";s:12:"ext_icon.gif";s:4:"2712";s:17:"ext_localconf.php";s:4:"cbf8";s:50:"Classes/Controller/Slot/AbstractSlotController.php";s:4:"e355";s:83:"Classes/Controller/Slot/Extensionmanager/ProcessActions/ModifiedFilesController.php";s:4:"a877";s:93:"Classes/Controller/Slot/Extensionmanager/ProcessActions/UpdateConfigurationFileController.php";s:4:"4eef";s:48:"Classes/Slot/Extensionmanager/ProcessActions.php";s:4:"2331";s:36:"Classes/Utility/ExtensionUtility.php";s:4:"ae5c";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"5363";s:26:"Documentation/Settings.yml";s:4:"03e2";s:46:"Documentation/Images/dialog_modified_files.jpg";s:4:"b064";s:40:"Documentation/Images/list_view_icons.jpg";s:4:"340f";s:36:"Documentation/Introduction/Index.rst";s:4:"69d5";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"c7c2";s:40:"Resources/Private/Language/locallang.xlf";s:4:"c492";s:42:"Resources/Public/Javascript/bindActions.js";s:4:"e9f3";}',
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Developer tools',
+  'description' => 'Adds nice developer tools to your TYPO3 installation',
+  'category' => 'be',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'CPS-IT GmbH',
+  'version' => '0.2.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.5.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"bcf6";s:12:"ext_icon.gif";s:4:"2712";s:17:"ext_localconf.php";s:4:"2796";s:50:"Classes/Controller/Slot/AbstractSlotController.php";s:4:"2764";s:83:"Classes/Controller/Slot/Extensionmanager/ProcessActions/ModifiedFilesController.php";s:4:"e56c";s:93:"Classes/Controller/Slot/Extensionmanager/ProcessActions/UpdateConfigurationFileController.php";s:4:"8a2e";s:48:"Classes/Slot/Extensionmanager/ProcessActions.php";s:4:"d0de";s:36:"Classes/Utility/ExtensionUtility.php";s:4:"f980";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"5363";s:26:"Documentation/Settings.yml";s:4:"03e2";s:46:"Documentation/Images/dialog_modified_files.jpg";s:4:"b064";s:40:"Documentation/Images/list_view_icons.jpg";s:4:"340f";s:36:"Documentation/Introduction/Index.rst";s:4:"69d5";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"c7c2";s:40:"Resources/Private/Language/locallang.xlf";s:4:"c492";s:42:"Resources/Public/Javascript/bindActions.js";s:4:"7cbb";}',
 );
 
