e89e08f22a46ef44d42efc968301da55583b4de5
Nicole Cordes <typo3@cordes.co>
2018-03-13 22:06:11 +0100

    [RELEASE] Release of devtools 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "devtools".
  *
- * Auto generated 20-02-2017 23:40
+ * Auto generated 13-03-2018 22:05
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author' => 'Nicole Cordes',
   'author_email' => 'typo3@cordes.co',
   'author_company' => 'CPS-IT GmbH',
-  'version' => '0.2.4',
+  'version' => '1.0.0',
   'constraints' => 
   array (
     'depends' => 
@@ -35,6 +35,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"b9b5";s:13:"composer.json";s:4:"836e";s:12:"ext_icon.gif";s:4:"2712";s:17:"ext_localconf.php";s:4:"2796";s:50:"Classes/Controller/Slot/AbstractSlotController.php";s:4:"2764";s:83:"Classes/Controller/Slot/Extensionmanager/ProcessActions/ModifiedFilesController.php";s:4:"e56c";s:93:"Classes/Controller/Slot/Extensionmanager/ProcessActions/UpdateConfigurationFileController.php";s:4:"217d";s:48:"Classes/Slot/Extensionmanager/ProcessActions.php";s:4:"d0de";s:36:"Classes/Utility/ExtensionUtility.php";s:4:"f980";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"5363";s:26:"Documentation/Settings.yml";s:4:"03e2";s:46:"Documentation/Images/dialog_modified_files.jpg";s:4:"b064";s:40:"Documentation/Images/list_view_icons.jpg";s:4:"340f";s:36:"Documentation/Introduction/Index.rst";s:4:"69d5";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"c7c2";s:40:"Resources/Private/Language/locallang.xlf";s:4:"c492";s:42:"Resources/Public/Javascript/bindActions.js";s:4:"9468";}',
+  '_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"b2e2";s:13:"composer.json";s:4:"9de6";s:12:"ext_icon.png";s:4:"a925";s:17:"ext_localconf.php";s:4:"e715";s:24:"sonar-project.properties";s:4:"3219";s:50:"Classes/Controller/Slot/AbstractSlotController.php";s:4:"7e78";s:83:"Classes/Controller/Slot/Extensionmanager/ProcessActions/ModifiedFilesController.php";s:4:"f9a4";s:93:"Classes/Controller/Slot/Extensionmanager/ProcessActions/UpdateConfigurationFileController.php";s:4:"a658";s:48:"Classes/Slot/Extensionmanager/ProcessActions.php";s:4:"82d3";s:36:"Classes/Utility/ExtensionUtility.php";s:4:"605d";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"5363";s:26:"Documentation/Settings.yml";s:4:"03e2";s:46:"Documentation/Images/dialog_modified_files.jpg";s:4:"b064";s:40:"Documentation/Images/list_view_icons.jpg";s:4:"340f";s:36:"Documentation/Introduction/Index.rst";s:4:"69d5";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"8b52";s:40:"Resources/Private/Language/locallang.xlf";s:4:"2fca";s:36:"Resources/Public/Icons/Extension.png";s:4:"a925";s:39:"Resources/Public/JavaScript/Devtools.js";s:4:"cc38";}',
 );
 
