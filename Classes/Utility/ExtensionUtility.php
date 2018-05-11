a34bc4c428e36ba31a8ba3f7905bdc46f80b8da1
Sven Friese <s.friese@familie-redlich.de>
2018-05-11 23:18:32 +0200

    [TASK] use private property in final class

--- Classes/Utility/ExtensionUtility.php
+++ Classes/Utility/ExtensionUtility.php
@@ -39,7 +39,7 @@ final class ExtensionUtility
     /**
      * @var PackageManager
      */
-    protected $packageManager;
+    private $packageManager;
 
     public function __construct(PackageManager $packageManager = null)
     {
