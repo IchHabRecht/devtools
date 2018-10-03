4a12844b2512e2f6291be47c010fc95f80d2dabc
Nicole Cordes <typo3@cordes.co>
2018-10-03 22:54:45 +0200

    [TASK] Raise TYPO3 dependencies to include 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'php' => '5.6.0-7.2.99',
-      'typo3' => '7.6.0-8.7.99',
+      'typo3' => '7.6.0-9.5.99',
     ),
     'conflicts' => 
     array (
